62433c3d41d3aa3fcea2a15ed2d1ce6b182e0293
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-07-22 13:05:27 +0530

    fix:stale-conversation when switching between conversation in the sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import Conversations from './pages/Conversations';
+import { useParams } from 'react-router-dom';
+function ConversationWrapper() {
+  const { conversationId } = useParams();
+  return <Conversations key={conversationId} />
+}
+
 function App() {
   return (
     <div className="dark w-full min-h-screen">
@@ -10,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/" element={<Dashboard />} />
-          <Route path="/conversation/:conversationId" element={<Conversations />} />
+          <Route path="/conversation/:conversationId" element={<ConversationWrapper />} />
         </Routes>
       </BrowserRouter>
     </div>
